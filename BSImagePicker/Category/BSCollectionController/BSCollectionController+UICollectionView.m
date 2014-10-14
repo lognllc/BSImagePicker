3d9188d75becbf4ce30185e44335aa0476adc986
Esteban Torres <me@estebantorr.es>
2014-10-13 21:42:36 -0600

    Added support to correctly update image numbers when deselecting "lower" numbers.
    Signed-off-by: Esteban Torres <me@estebantorr.es>

--- BSImagePicker/Category/BSCollectionController/BSCollectionController+UICollectionView.m
+++ BSImagePicker/Category/BSCollectionController/BSCollectionController+UICollectionView.m
@@ -28,93 +28,93 @@
 @implementation BSCollectionController (UICollectionView)
 
 - (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section {
-    NSInteger items = 0;
-
-    if(self.collectionModel) {
-        items = [self.collectionModel numberOfItemsInSection:section];
-    }
-
-    return items;
+	NSInteger items = 0;
+	
+	if(self.collectionModel) {
+		items = [self.collectionModel numberOfItemsInSection:section];
+	}
+	
+	return items;
 }
 
 - (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath {
-    BSPhotoCell *cell = (BSPhotoCell *)[self.collectionCellFactory cellAtIndexPath:indexPath forCollectionView:collectionView withModel:self.collectionModel];
-
-    if([self.collectionModel isItemAtIndexPathSelected:indexPath]) {
-        [collectionView selectItemAtIndexPath:indexPath animated:NO scrollPosition:UICollectionViewScrollPositionNone];
-		[cell setPictureNumber:[self.collectionModel.selectedItems count] selected:YES animated:NO];
-    }
-
-    return cell;
+	BSPhotoCell *cell = (BSPhotoCell *)[self.collectionCellFactory cellAtIndexPath:indexPath forCollectionView:collectionView withModel:self.collectionModel];
+	
+	if([self.collectionModel isItemAtIndexPathSelected:indexPath]) {
+		[collectionView selectItemAtIndexPath:indexPath animated:NO scrollPosition:UICollectionViewScrollPositionNone];
+		[cell setPictureNumber:0 selected:NO animated:NO];
+		[cell setPictureNumber:[self.collectionModel.selectedItems count] selected:YES animated:YES];
+	}
+	
+	return cell;
 }
 
 - (NSInteger)numberOfSectionsInCollectionView:(UICollectionView *)collectionView {
-    NSInteger sections = 0;
-
-    if(self.collectionModel) {
-        sections = [self.collectionModel numberOfSections];
-    }
-
-    return sections;
+	NSInteger sections = 0;
+	
+	if(self.collectionModel) {
+		sections = [self.collectionModel numberOfSections];
+	}
+	
+	return sections;
 }
 
 - (BOOL)collectionView:(UICollectionView *)collectionView shouldSelectItemAtIndexPath:(NSIndexPath *)indexPath {
-    return [self.collectionModel.selectedItems count] < [[BSImagePickerSettings sharedSetting] maximumNumberOfImages] && !self.disableSelection;
+	return [self.collectionModel.selectedItems count] < [[BSImagePickerSettings sharedSetting] maximumNumberOfImages] && !self.disableSelection;
 }
 
 - (BOOL)collectionView:(UICollectionView *)collectionView shouldDeselectItemAtIndexPath:(NSIndexPath *)indexPath {
-    return !self.disableSelection;
+	return !self.disableSelection;
 }
 
 - (void)collectionView:(UICollectionView *)collectionView didDeselectItemAtIndexPath:(NSIndexPath *)indexPath {
-    //Remove item
-    [self.collectionModel deselectItemAtIndexPath:indexPath];
-    
-    BSPhotoCell *cell = (BSPhotoCell *)[collectionView cellForItemAtIndexPath:indexPath];
-	[cell setPictureNumber:[self.collectionModel.selectedItems count] selected:NO animated:YES];
-
-    if([[BSImagePickerSettings sharedSetting] toggleBlock]) {
-        ALAsset *asset = [self.collectionModel itemAtIndexPath:indexPath];
-        [BSImagePickerSettings sharedSetting].toggleBlock(asset, NO);
-    }
+	//Remove item
+	[self.collectionModel deselectItemAtIndexPath:indexPath];
+	
+	if([[BSImagePickerSettings sharedSetting] toggleBlock]) {
+		ALAsset *asset = [self.collectionModel itemAtIndexPath:indexPath];
+		[BSImagePickerSettings sharedSetting].toggleBlock(asset, NO);
+	}
+	
+	[self.collectionView reloadData];
 }
 
 - (void)collectionView:(UICollectionView *)collectionView didSelectItemAtIndexPath:(NSIndexPath *)indexPath {
-    //Add item
-    [self.collectionModel selectItemAtIndexPath:indexPath];
-    
-    BSPhotoCell *cell = (BSPhotoCell *)[collectionView cellForItemAtIndexPath:indexPath];
+	//Add item
+	[self.collectionModel selectItemAtIndexPath:indexPath];
+	
+	BSPhotoCell *cell = (BSPhotoCell *)[collectionView cellForItemAtIndexPath:indexPath];
 	[cell setPictureNumber:[self.collectionModel.selectedItems count] selected:YES animated:YES];
-
-    if([[BSImagePickerSettings sharedSetting] toggleBlock]) {
-        ALAsset *asset = [self.collectionModel itemAtIndexPath:indexPath];
-        [BSImagePickerSettings sharedSetting].toggleBlock(asset, YES);
-    }
+	
+	if([[BSImagePickerSettings sharedSetting] toggleBlock]) {
+		ALAsset *asset = [self.collectionModel itemAtIndexPath:indexPath];
+		[BSImagePickerSettings sharedSetting].toggleBlock(asset, YES);
+	}
 }
 
 - (CGSize)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout*)collectionViewLayout sizeForItemAtIndexPath:(NSIndexPath *)indexPath {
-    CGSize itemSize = CGSizeZero;
-
-    if(self.collectionCellFactory) {
-        itemSize = [[self.collectionCellFactory class]
-                    sizeAtIndexPath:indexPath
-                    forCollectionView:collectionView
-                    withModel:self.collectionModel];
-    }
-
-    return itemSize;
+	CGSize itemSize = CGSizeZero;
+	
+	if(self.collectionCellFactory) {
+		itemSize = [[self.collectionCellFactory class]
+					sizeAtIndexPath:indexPath
+					forCollectionView:collectionView
+					withModel:self.collectionModel];
+	}
+	
+	return itemSize;
 }
 
 - (UIEdgeInsets)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout*)collectionViewLayout insetForSectionAtIndex:(NSInteger)section {
-    return [[self.collectionCellFactory class] edgeInsetAtSection:section forCollectionView:collectionView withModel:self.collectionModel];
+	return [[self.collectionCellFactory class] edgeInsetAtSection:section forCollectionView:collectionView withModel:self.collectionModel];
 }
 
 - (CGFloat)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout*)collectionViewLayout minimumLineSpacingForSectionAtIndex:(NSInteger)section {
-    return [[self.collectionCellFactory class] minimumLineSpacingAtSection:section forCollectionView:collectionView withModel:self.collectionModel];
+	return [[self.collectionCellFactory class] minimumLineSpacingAtSection:section forCollectionView:collectionView withModel:self.collectionModel];
 }
 
 - (CGFloat)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout*)collectionViewLayout minimumInteritemSpacingForSectionAtIndex:(NSInteger)section {
-    return [[self.collectionCellFactory class] minimumItemSpacingAtSection:section forCollectionView:collectionView withModel:self.collectionModel];
+	return [[self.collectionCellFactory class] minimumItemSpacingAtSection:section forCollectionView:collectionView withModel:self.collectionModel];
 }
 
 @end
